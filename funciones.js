3c2604cd4ef3b9e5cb8205ba0cdcc477fe11d294
tumami7654321@gmail.com <tumami7654321@gmail.com>
2024-04-05 22:34:51 -0300

    * UPD: Update 'libros' and js/css

--- funciones.js
+++ funciones.js
@@ -1,30 +1,26 @@
 function pintarNavBar() {
     var navbar = `
     <nav class="navbar navbar-expand-lg navbar-personalizado">
-        <a class="navbar-brand" href="#">Bienvenido/a</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="index.html">Inicio</a>
+                    <a class="nav-link nav-link-personalizado" href="index.html">Inicio</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="libros.html">Libros</a>
+                    <a class="nav-link nav-link-personalizado" href="libros.html">Libros</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="nosotros.html">Nosotros</a>
+                    <a class="nav-link nav-link-personalizado" href="nosotros.html">Nosotros</a>
                 </li>
             </ul>
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="registro.html">Registrar</a>
+                    <a class="nav-link nav-link-personalizado" href="registro.html">Registrar</a>
                 </li>
             </ul>
         </div>
     </nav>
     `;
 
-    document.body.innerHTML += navbar;
+    document.getElementById('navbar').innerHTML = navbar;
 }
