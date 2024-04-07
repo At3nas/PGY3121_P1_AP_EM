bee0973e3c5f18ceac2850145fdb94f1d1848658
Atenas Pérez <atekapeko@gmail.com>
2024-04-07 11:51:38 -0400

    * ADD: Home page

--- funciones.js
+++ funciones.js
@@ -1,7 +1,7 @@
 function pintarNavBar() {
     var navbar = `
     <nav class="navbar navbar-expand-lg navbar-personalizado">
-        <a class="navbar-brand" href="#">Bienvenido/a</a>
+        <a class="navbar-brand" href="#">BiblioMarket</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
